f0a8f89b7da71e80d574f64abedd44a1e95d331b
Philipp <kol18485@spengergasse.at>
2019-03-13 20:30:42 +0100

    Produt.java

---
+++ Product.java
@@ -0,0 +1,21 @@
+public class Product
+{
+	private String name;
+
+	public Product()
+	{
+		setName("Coca Cola");
+	}
+
+	public void setName(String name)
+	{
+		this.name=name;
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+
+
+}
