e4182273d21658d14011da3a44dc4f71b4c41dc6
Philipp <kol18485@spengergasse.at>
2019-03-15 02:11:54 +0100

    company Name

--- company.java
+++ company.java
@@ -1,11 +1,23 @@
 public class Company
 {
 	private ArrayList<Product> products;
+	private String name;
 
 	public Company()
 	{
 		products = new ArrayList<Product>();
+		setName("IBM");
 	}
 
+
+	public void setName(String name)
+	{
+		this.name=name;
+	}	
+
+	public String getName()
+	{
+		return name;
+	}
 }
 
